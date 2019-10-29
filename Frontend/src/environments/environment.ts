5e303319903a2f9cae99d394c8a257817c1d553f
Joel-Vitelli <jvitelli92@gmail.com>
2019-10-29 17:08:56 -0300

    Update Enviroments

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAkav3JbZOTbxMFtfnL_UgV712ioO2htmM",
-    authDomain: "libreria-8a439.firebaseapp.com",
-    databaseURL: "https://libreria-8a439.firebaseio.com",
-    projectId: "libreria-8a439",
-    storageBucket: "libreria-8a439.appspot.com",
-    messagingSenderId: "52803251337",
-    appId: "1:52803251337:web:e6446c7db06a17ac09ea58"
+    apiKey: "<<<<>>>>",
+    authDomain: "<<<<>>>>",
+    databaseURL: "<<<<>>>>",
+    projectId: "<<<<>>>>",
+    storageBucket: "<<<<>>>>",
+    messagingSenderId: "<<<<>>>>",
+    appId: "<<<<>>>>"
   }
 };
 
